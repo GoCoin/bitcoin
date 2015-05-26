6aeeb2e9b40f4cb312e07c9ae059fb08675ae5b0
Scott Spangenberg <sjspangenberg@gmail.com>
2015-05-26 15:07:05 -0400

    Added Reverse() and GetReverseHex() for ZiftrPay RPC

--- src/uint256.h
+++ src/uint256.h
@@ -12,6 +12,7 @@
 #include <stdint.h>
 #include <string>
 #include <vector>
+#include <stdio.h>
 
 class uint_error : public std::runtime_error {
 public:
@@ -230,6 +231,20 @@ public:
     void SetHex(const std::string& str);
     std::string ToString() const;
 
+    // S.M. Added reversing convenience methods
+    std::string GetReverseHex() const
+    {
+        char psz[sizeof(pn)*2 + 1];
+        for (unsigned int i = 0; i < sizeof(pn); i++)
+            sprintf(psz + i*2, "%02x", ((unsigned char*)pn)[i]);
+        return std::string(psz, psz + sizeof(pn)*2);
+    }
+
+    void Reverse(base_uint& result) const
+    {
+        result.SetHex(this->GetReverseHex());
+    }
+
     unsigned char* begin()
     {
         return (unsigned char*)&pn[0];
@@ -303,7 +318,7 @@ public:
     uint256(uint64_t b) : base_uint<256>(b) {}
     explicit uint256(const std::string& str) : base_uint<256>(str) {}
     explicit uint256(const std::vector<unsigned char>& vch) : base_uint<256>(vch) {}
-    
+
     /**
      * The "compact" format is a representation of a whole
      * number N using an unsigned 32bit number similar to a
@@ -313,12 +328,12 @@ public:
      * The lower 23 bits are the mantissa.
      * Bit number 24 (0x800000) represents the sign of N.
      * N = (-1^sign) * mantissa * 256^(exponent-3)
-     * 
+     *
      * Satoshi's original implementation used BN_bn2mpi() and BN_mpi2bn().
      * MPI uses the most significant bit of the first byte as sign.
      * Thus 0x1234560000 is compact (0x05123456)
      * and  0xc0de000000 is compact (0x0600c0de)
-     * 
+     *
      * Bitcoin only uses this "compact" format for encoding difficulty
      * targets, which are unsigned 256bit quantities.  Thus, all the
      * complexities of the sign bit and using base 256 are probably an
